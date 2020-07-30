d2ffad90656de1b20bc67f43dd4cf3a508a93325
Francis Eclevia <65243967+francisecleviaCI@users.noreply.github.com>
2020-07-29 22:00:38 -0400

    just started

--- main.js
+++ main.js
@@ -1,5 +1,41 @@
 const Ray = function() {
-  
+  return {
+    length: 0,
+    push: function(value){
+    this.length = this.length + 1;
+    },
+
+    pop: function(){
+    this.length = this.length - 1;    
+    },
+
+    includes: function(){
+
+    },
+    indexOf: function(){
+
+    },
+    shift: function(){
+
+    },
+    unshift: function(){
+
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
 }
 
 
